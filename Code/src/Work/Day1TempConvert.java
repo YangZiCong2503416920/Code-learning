632db518f5fd2e24d6e0c2a1bd4a828c16acf478
Yang ZiCong <YangZC2503416920@163.com>
2025-10-21 11:52:06 +0800

    Create Day1TempConvert.java

---
+++ Code/src/Work/Day1TempConvert.java
@@ -0,0 +1,16 @@
+package Work;
+
+import java.util.Scanner;
+
+public class Day1TempConvert {
+    public static void main(String[] args) {
+        System.out.print("请输入温度（摄氏度）：");
+        Scanner sc = new Scanner(System.in);
+        double userTemp = sc.nextInt();
+        double F;
+        F = userTemp * 9/5 + 32;
+        System.out.println("您输入的温度转化为华氏度为：" + F + "F");
+
+
+    }
+}
