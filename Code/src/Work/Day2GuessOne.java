d98f99ae457336d70113e52f5447f968617a3fec
Yang ZiCong <88575361+YangZiCong2503416920@users.noreply.github.com>
2025-10-22 20:55:00 +0800

    Create Day2GuessOne.java
    一个单次猜数字游戏

---
+++ Code/src/Work/Day2GuessOne.java
@@ -0,0 +1,21 @@
+package Work;
+
+import java.util.Scanner;
+
+public class Day2GuessOne {
+    static void main(String[] args) {
+        int secret = new java.util.Random().nextInt(100) + 1;   // 1~100
+        System.out.print("请输入你猜测的数字：");
+        Scanner sc = new Scanner(System.in);
+        int guess = sc.nextInt();
+        if (secret < guess) {
+            System.out.println("大了");
+        }   else if (secret > guess) {
+            System.out.println("小了");
+        }   else if (secret == guess) {
+            System.out.println("答对了");
+        }
+        System.out.print("题目答案是：");
+        System.out.println(secret);
+    }
+}
